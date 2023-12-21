7c54b1225ce00dd97b7499b2f6bb300173be1fee
Ofodile Justin <justinofodile@gmail.com>
2023-12-21 11:28:16 +0100

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { readFileSync } = require('fs');
 const path = require('path')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { parse } = require('node-html-parser');
 
 
